5038b0e07c4ebb7d03e03f0383f9f42a6d82a5c6
lewis <788415@swansea.ac.uk>
2017-01-14 08:15:49 +0000

    Images + desc

---
+++ config/Migrations/20170114075435_module_images.php
@@ -0,0 +1,59 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ModuleImages extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up(){
+      $moduleTable = $this->table('module');
+
+      $moduleTable
+        ->addColumn('banner', 'string', [
+          'length' => 255,
+          'default' => null,
+          'null' => false
+        ])
+        ->addColumn('icon', 'string', [
+          'length' => 255,
+          'default' => null,
+          'null' => false
+        ])
+        ->addColumn('description', 'string', [
+          'length' => 255,
+          'default' => null,
+          'null' => false
+        ])
+        ->update();
+    }
+
+    public function down(){
+      $moduleTable = $this->table('module');
+
+      $moduleTable
+        ->removeColumn('banner')
+        ->removeColumn('icon')
+        ->removeColumn('description')
+        ->update();
+    }
+}
