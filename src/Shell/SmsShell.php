5cc9ac1367b2a6a1ef3501bc6b7c501f6af1e602
CJSmi <CJSmi@users.noreply.github.com>
2017-01-30 16:53:03 +0000

    Update twilio details, added inactive message.

--- src/Shell/SmsShell.php
+++ src/Shell/SmsShell.php
@@ -1,5 +1,6 @@
 <?php
 //TODO: Use composer for library
+//TODO: Comments and tidy
 namespace App\Shell;
 use \DateTime;
 
@@ -11,10 +12,13 @@ require_once(APP . 'Vendor' . DS . 'Twilio' . DS . 'autoload.php');
 use Twilio\Rest\Client;
 
 define('SMS', 1);
-define('TWILIO_NUM', '+441675292021');
-define('TWILIO_SID', 'ACe11a31f43ea0853ee364beeeb29eb325');
-define('TWILIO_TOK', '2e6cffca227d4cd629e9bf87bb2ef48b');
-define('WELCOME', 'Welcome %name% to Champions for Health. We\'ll send you a weekly text message containing tips, updates, and links to resources which we hope will help you with your challenges. If you\'d prefer not to receive these messages, reply with STOP. All the best, The C4H Team');
+define('TWILIO_NUM', '+442820032179');
+define('TWILIO_SID', 'AC199af5e0e704534d8df940fd07c6c580');
+define('TWILIO_TOK', 'c22ba75cef47d4a208aa93eb02779b73');
+define('WELCOME_MSG', 'Welcome %name% to Champions for Health. We\'ll send you a weekly text message containing tips, updates, and links to resources which we hope will help you with your challenges. If you\'d prefer not to receive these messages, reply with STOP. All the best, The C4H Team');
+define('INACTIVE_TIME', '-1 week');
+define('INACTIVE_MSG', '');
+define('TIME_FORMAT', 'Y-m-d\TH:i:sP');
 
 class SmsShell extends Shell{
 
@@ -22,7 +26,7 @@ class SmsShell extends Shell{
 		$table = TableRegistry::get('Messages');
 		$date = new DateTime();
 		$query = $table->find()
-			->where(['sent' => false, 'type' => SMS, 'scheduled_time <=' => $date->format('Y-m-d\TH:i:sP')]);
+			->where(['sent' => false, 'type' => SMS, 'scheduled_time <=' => $date->format(TIME_FORMAT)]);
 
 		foreach($query as $msg){
 			try{
@@ -41,53 +45,87 @@ class SmsShell extends Shell{
 		$client = new Client(TWILIO_SID, TWILIO_TOK);
 		
 		foreach($query as $user){
-			try{
 			$to = $user->phone_number;
 			$name = TableRegistry::get('Users')->get($user->user_id)->username;
 			$msg = str_replace('%name%', $name, $message);
-			
-			$client->messages->create(
-				$to,
-				array('from' => TWILIO_NUM,
-					'body' => $msg
-				)
-			);
-			} catch(Exception $e){
-				Log::write('error', 'SMS failed: ' . $e);
-			}
-		}
-	}
-	
-	public function welcome(){
-		$tableUsers = TableRegistry::get('Users');
-		$date = new DateTime();
-		$date->modify('-3 hours');
-		$query = $tableUsers->find()->where(['created >=' => $date->format('Y-m-d\TH:i:sP')]);
-		$client = new Client(TWILIO_SID, TWILIO_TOK);
-		
-		foreach($query as $user){
-			$profile = TableRegistry::get('Profile')->find()->where(['user_id' => $user->id])->first();
-			$name = $user->username;
-			$to = $profile->phone_number;
-			$msg = str_replace('%name%', $name, WELCOME);
-			
-			try{
+			if($to && $to != ''){
+				try{
 				$client->messages->create(
 					$to,
 					array('from' => TWILIO_NUM,
 						'body' => $msg
 					)
 				);
-			}catch(Exception $e){
-				Log::write('error', 'SMS Welcome failed: ' . $e);
+				} catch(Exception $e){
+					Log::write('error', 'SMS failed: ' . $e);
+				}
+			}
+		}
+	}
+	
+	public function welcome(){
+		$tableUsers = TableRegistry::get('Users');
+		$query = $tableUsers->find()->where(['received_welcome_sms' => false]);
+		$client = new Client(TWILIO_SID, TWILIO_TOK);
+		
+		foreach($query as $user){
+			$profile = TableRegistry::get('Profile')->find()
+						->where(['user_id' => $user->id])->first();
+			$name = $user->username;
+			$to = $profile['phone_number'];
+			debug($profile);
+			if($to && $to != ''){
+				try{
+					$msg = str_replace('%name%', $name, WELCOME_MSG);
+					$client->messages->create(
+						$to,
+						array('from' => TWILIO_NUM,
+							'body' => $msg
+						)
+					);
+					$user->received_welcome_sms = true;
+					if(!$tableUsers->save($user)){
+						Log::write('error', 'Welcome SMS status failed: ' . $user);
+					}
+				}catch(Exception $e){
+					Log::write('error', 'Welcome SMS failed: ' . $e);
+				}
 			}
 		}
 	}
 	
 	public function inactive(){
-		//TODO: Message when inactive, needs DB.
+		$tableUsers = TableRegistry::get('Users');
+		$date = new DateTime();
+		$date->modify(INACTIVE_TIME);
+		$query = $tableUsers->find()
+					->where(['received_inactive_sms' => false, 'last_logged_in <=' => $date->format(TIME_FORMAT)]);
+		
+		foreach($query as $user){
+			$profile = TableRegistry::get('Profile')->find()
+							->where(['user_id' => $user->id])->first();
+			
+			$to = $profile->phone_number;
+			$name = $user->username;
+			try{
+				$msg = str_replace('%name%', $name, INACTIVE_MSG);
+				$client->messages->create(
+					$to,
+					array('from' => TWILIO_NUM,
+						'body' => $msg
+					)
+				);
+				$user->received_inactive_sms = true;
+				if(!$tableUsers->save($user)){
+					Log::write('error', 'Inactive SMS status failed: ' . $user);
+				}
+			}catch(Exception $e){
+				Log::write('error', 'Inactive SMS failed: profile(' . $profile->id . '), phone_number(' . $profile->phone_number . ')');
+			}
+	}
 	}
 	
+	//Schedule an sms to be sent
 	//Time Format: mm/dd/yyyyThh:mm:ss
 	public function schedule($message = '', $time){
 		$date = new DateTime($time);
@@ -103,6 +141,21 @@ class SmsShell extends Shell{
 
 		if(!$table->save($newMsg)){
 			Log::write('error', 'SMS schedule fail: ' . $newMsg);
+		}
+	}
+	
+	//Check if account has become active after being inactive.
+	public function active(){
+		$tableUsers = TableRegistry::get('Users');
+		$date = new DateTime();
+		$date->modify(INACTIVE_TIME);
+		$query = $tableUsers->find()
+					->where(['received_inactive_sms' => true, 'last_logged_in >' => $date->format(TIME_FORMAT)]);
+		foreach($query as $user){
+			$user->received_inactive_sms = false;
+			if(!$tableUsers->save($user)){
+				Log::write('error', 'Inactive SMS status failed: ' . $user);
+			}
 		}
 	}
 }
