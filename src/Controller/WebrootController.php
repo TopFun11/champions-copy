3eabd441823ec1b7b71c3eea6f02f9b7e85f3b88
Leslie <32874682+AprilLeslie@users.noreply.github.com>
2018-03-21 11:41:23 +0000

    Update WebrootController.php

--- src/Controller/WebrootController.php
+++ src/Controller/WebrootController.php
@@ -6,5 +6,11 @@ use App\Controller\AppController;
 
 class WebrootController extends AppController
 {
-
+  public function initialize(){
+       parent::initialize();
+       $this->loadModel('File');
+  }
+  public function Relaxtion(){
+    $this->loadComponent(phwcfh/webroot/uploads/Relaxationtipstodownload.pdf)
+  }
 }
