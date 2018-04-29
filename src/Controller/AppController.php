5486a77130882af84aa19f760444f50194b9a9db
Blitheness <blitheness@oneiric.co.uk>
2018-04-29 19:10:03 +0100

    Change login destination to stop warnings for regular users, resolves #9

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -44,10 +44,7 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
         $this->loadComponent('Auth', [
-          'loginRedirect' => [
-            'controller' => 'module',
-            'action' => 'index',
-          ],
+          'loginRedirect' => '/',
           'logoutRedirect' => [
             'controller' => 'Pages',
             'action' => 'display',
