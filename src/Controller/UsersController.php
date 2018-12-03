a9f484b98892dbe2fc08a2da4edc3355a93a29de
TopFun11 <37330262+TopFun11@users.noreply.github.com>
2018-12-03 14:40:03 +0000

    Update UsersController.php

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -81,7 +81,7 @@ use Cake\Controller\Component\AuthComponent;
                  }
 
                  $profile->points = $profile->points + 1;
-                 $profile->logon = $profile->logon + 1;
+                 $profile->logCount = $profile->logCount + 1;
                  $this->Profile->save($profile);
 
                  return $this->redirect($this->Auth->redirectUrl());
