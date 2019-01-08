297db2ce04af2ecc84213a36368c25dec35956aa
TopFun11 <37330262+TopFun11@users.noreply.github.com>
2019-01-08 14:18:19 +0000

    Update ProfileController.php

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -52,7 +52,7 @@ class ProfileController extends AppController
         $user= $this->Users->find("all")->where(['id' => $this->Auth->user("id")])->first();
         
         $recordset = $this->Recordset->find("all")->where(['user_id' => $id]);
-        $record = $this->Record->find("all")->where(has($id));
+        $record = $this->Record->find("all")->where(['recordset_id' => $this->Recordset("id")]);
 
         $this->set('profile', $profile);
         $this->set('user', $user);
