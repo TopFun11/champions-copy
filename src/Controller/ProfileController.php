911f9839afb46985402775740554d998871e74d2
TopFun11 <37330262+TopFun11@users.noreply.github.com>
2019-01-08 14:51:13 +0000

    Update ProfileController.php

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -53,13 +53,11 @@ class ProfileController extends AppController
         
         $recordset = $this->Recordset->find("all")->where(['user_id' => $id]);
         $rid = $this->Auth->recordset("user_id");
-        $record = $this->Record->find("all")->where(['recordset_id' => $rid]);
 
         $this->set('profile', $profile);
         $this->set('user', $user);
         $this->set('recordset', $recordset);
-        $this->set('record', $record);
-        $this->set('_serialize', ['profile', 'user', 'recordset', 'record']);
+        $this->set('_serialize', ['profile', 'user', 'recordset']);
     }
 
     /**
