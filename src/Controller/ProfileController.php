3bf82da9e6300abb78efcc75247f2d2b6b5576a9
TopFun11 <37330262+TopFun11@users.noreply.github.com>
2019-01-08 13:41:48 +0000

    Update ProfileController.php

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -16,6 +16,9 @@ class ProfileController extends AppController
     {
       parent::initialize();
       $this->loadModel("Users");
+      $this->loadModel("Recordset");
+      $this->loadModel("Record");
+      $this->loadModel("Exercise");
     }
     /**
      * Index method
@@ -45,9 +48,32 @@ class ProfileController extends AppController
         $id = $this->Auth->user("id");
         $profile = $this->Profile->find("all")->where(['user_id' => $this->Auth->user("id")])->first();
         $user= $this->Users->find("all")->where(['id' => $this->Auth->user("id")])->first();
+        
+        $record = $this->Record->get($id, [
+            'contain' => ['Recordset']
+        ]);
+
+        
+        $recordset = null;
+        $tmp = $this->Recordset->get($id, [
+            'contain' => [],
+        ]);
+        if($tmp->exercise_id != null){
+          $recordset = $this->Recordset->get($id, [
+              'contain' => ['Exercise', 'Users'],
+          ]);
+          $this->set('recordset', $recordset);
+        }else{
+          $recordset = $this->Recordset->get($id, [
+              'contain' => ['Screener' => ['Formular'], 'Users', 'Record'],
+          ]);
+        }
+        
+        $this->set('recordset', $recordset);
+        $this->set('record', $record);
         $this->set('profile', $profile);
         $this->set('user', $user);
-        $this->set('_serialize', ['profile', 'user']);
+        $this->set('_serialize', ['profile', 'user', 'record']);
     }
 
     /**
